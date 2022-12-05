87c0dbea67efd1039973b79adb073cc11083e34c
sharonshiny79 <119587041+sharonshiny79@users.noreply.github.com>
2022-12-05 11:05:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const addDays = require("date-fns/addDays");
 
-const getDateAfterXDays = (days) => {
-  const newDate = addDays(new Date(2022, 11, 5), 100);
-  return `${newDate.getDate()}-${newDate.getMonth() + 1}-
-  ${newDate.getFullYear()}`;
-};
-module.exports = getDateAfterXDays;
-
 const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  let date = newDate();
-  response.send(getDateAfterXDays());
+ const newDate = addDays(new Date(2022, 11, 5), 100);
+  return `${newDate.getDate()}-${newDate.getMonth() + 1}-
+  ${newDate.getFullYear()}`);
 });
 app.listen(3000);
 
